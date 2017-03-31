92263f6aa018311032d0ebd8980fd75e44fab1ae
William Ku <kucheech@gmail.com>
2017-03-31 10:18:52 +0800

    demo FeedReader app

--- assets/www/js/app.js
+++ assets/www/js/app.js
@@ -3,13 +3,60 @@ document.addEventListener('deviceready', onDeviceReady, false);
 
 // Cordova is ready
 function onDeviceReady() {
+
 //    alert("onDeviceReady"); //
-    navigator.notification.alert(
-        'Welcome to TMC',  // message
-        alertDismissed,         // callback
-        'Hi',            // title
-        'OK'                  // buttonName
-    );
+//    navigator.notification.alert(
+//        'Welcome to TMC',  // message
+//        alertDismissed,         // callback
+//        'Hi',            // title
+//        'OK'                  // buttonName
+//
+//    );
+
+jQuery.getFeed({
+    url: "http://planet.gnome.org/rss20.xml",
+    success: function(feed) {
+        for (var i = 0; i < feed.items.length; i++) {
+            var item = feed.items[i];
+            console.log("Item " + i + ": " + item.title);
+            appendPost(item);
+        }
+
+        $("#posts").listview("refresh");
+    },
+    error: function(error) {
+        console.log("error in accessing feed");
+    }
+});
+
+
+
+
+
+}
+
+
+
+function getFeed(feed) {
+        for (var i = 0; i < feed.items.length; i++) {
+            var item = feed.items[i];
+            appendPost(item);
+        }
+
+        $("#posts").listview("refresh");
+
+}
+
+
+
+function appendPost(item) {
+//    $("#posts").append("<li>" + item.title + "</li>");
+var link = $("<a />").attr("href", item.link);
+$("<h3 />").append(item.title).appendTo(link);
+$("<p />").append(item.updated).appendTo(link);
+var li = $("<li />").append(link);
+$("#posts").append(li);
+
 }
 
 
